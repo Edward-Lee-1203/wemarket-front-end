422a2bac474ecfa6de6be964e0de3eeb31744b4d
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2021-05-18 12:39:00 +0700

    done all buyer layouts

--- WeMarketAndroid/app/src/main/java/com/example/wemarketandroid/views/buyer/IHideBottomNavBar.java
+++ WeMarketAndroid/app/src/main/java/com/example/wemarketandroid/views/buyer/IHideBottomNavBar.java
@@ -5,3 +5,4 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public interface IHideBottomNavBar {
     void hideBottomNavBar(BottomNavigationView bottomNavigationView);
 }
+//TODO: transform these interfaces into attributes if possible
